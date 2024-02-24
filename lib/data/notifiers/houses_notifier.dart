97ea69ee4569726d1a41643e27a3fce356b2f4e4
LodesHBeyers <lodesbeyers1@gmail.com>
2024-02-24 14:10:56 +0200

    Add houses and current house notifiers

---
+++ lib/data/notifiers/houses_notifier.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wizard_world/data/entities/house.dart';
+import 'package:wizard_world/data/repositories/houses_repository.dart';
+
+class HousesNotifier extends AsyncNotifier<List<House>> {
+  @override
+  FutureOr<List<House>> build() {
+    return loadHouses();
+  }
+
+  Future<List<House>> loadHouses() async {
+    try{
+      return ref.read(housesRepositoryProvider).getAllHouses();
+    }catch(e,s){
+      rethrow;
+    }
+  }
+}
+
+final AsyncNotifierProvider<HousesNotifier, List<House>> housesProvider =
+    AsyncNotifierProvider<HousesNotifier, List<House>>(
+  HousesNotifier.new,
+);
