940f3b2bdd651a5a12fe80e65800e9b88d2198eb
LodesHBeyers <lodesbeyers1@gmail.com>
2024-02-27 17:37:34 +0200

    Flesh out theming, add dependancies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wizard_world/data/entities/house.dart';
-import 'package:wizard_world/data/notifiers/current_house_notifier.dart';
+import 'package:wizard_world/data/notifiers/houses/current_house_notifier.dart';
 import 'package:wizard_world/globals/globals_keys.dart';
 import 'package:wizard_world/services/routing/app_router.dart';
 import 'package:wizard_world/services/routing/app_routes.dart';
@@ -24,7 +24,7 @@ class MyApp extends ConsumerWidget {
     return MaterialApp(
       title: 'Wizard World',
       navigatorKey: globalNavigatorKey,
-      theme: AppTheme.buildTheme(
+      theme: AppTheme.fromHouse(
         currentHouse,
       ),
       onGenerateRoute: AppRouter.onGenerateRoute,
