1529f6cd12516a689ecb2d9811976ccdf3a410a6
LodesHBeyers <lodesbeyers1@gmail.com>
2024-03-01 18:25:41 +0200

    Accept that I'm no esigner. Use generated theme

--- lib/presentation/theme/app_theme.dart
+++ lib/presentation/theme/app_theme.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:wizard_world/presentation/components/assets/app_fonts.dart';
+import 'package:wizard_world/presentation/theme/color_schemes.g.dart';
 
 class AppTheme {
   static ThemeData get light => ThemeData(
         useMaterial3: true,
         fontFamily: AppFonts.eduTAS,
+        colorScheme: lightColorScheme,
+      ).copyWith(
+        appBarTheme: AppBarTheme(
+          color: lightColorScheme.inversePrimary,
+          titleTextStyle: const TextStyle(
+            fontSize: 32,
+            fontFamily: AppFonts.magicSchool,
+            color: Color.fromARGB(255, 31, 31, 31),
+          ),
+        ),
+      );
+
+  static ThemeData get dark => ThemeData(
+        useMaterial3: true,
+        fontFamily: AppFonts.eduTAS,
+        colorScheme: darkColorScheme,
       ).copyWith(
         appBarTheme: const AppBarTheme(
           titleTextStyle: TextStyle(
@@ -14,6 +31,4 @@ class AppTheme {
           ),
         ),
       );
-
-  static ThemeData get dark => ThemeData();
 }
