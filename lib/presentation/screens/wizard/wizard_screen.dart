8a171c1854c93033c977f4390345757519334089
LodesHBeyers <lodesbeyers1@gmail.com>
2024-03-02 11:39:26 +0200

    Add placeholder wizard screen

---
+++ lib/presentation/screens/wizard/wizard_screen.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+import 'package:wizard_world/data/entities/wizard.dart';
+import 'package:wizard_world/presentation/components/app_bar/styled_app_bar.dart';
+
+class WizardScreen extends StatelessWidget {
+  final Wizard wizard;
+  const WizardScreen({
+    super.key,
+    required this.wizard,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        appBarTheme: AppBarTheme(
+          titleTextStyle: Theme.of(context).textTheme.titleLarge?.copyWith(
+                fontWeight: FontWeight.w600,
+                letterSpacing: 1.25,
+              ),
+        ),
+      ),
+      child: Scaffold(
+        appBar: StyledAppBar(
+          heading: wizard.fullName,
+        ),
+      ),
+    );
+  }
+}
