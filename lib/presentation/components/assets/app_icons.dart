b88cda6ffcbd5d85d3f86c5981dac4d3298871a1
LodesHBeyers <lodesbeyers1@gmail.com>
2024-03-02 11:36:43 +0200

    Add assets for house

--- lib/presentation/components/assets/app_icons.dart
+++ lib/presentation/components/assets/app_icons.dart
@@ -1,4 +1,19 @@
 
+// ignore_for_file: constant_identifier_names
+
 class AppIcons{
-  static String witchHat = "assets/icons/witch_hat.png";
+  static const String castle_filled = "assets/icons/ic_castle_filled.png";
+  static const String magic_wand_filled = "assets/icons/ic_magic_wand_filled.png";
+  static const String potion_filled = "assets/icons/ic_potion_filled.png";
+  static const String wizard_hat_filled = "assets/icons/ic_wizard_hat_filled.png";
+  static const String water_element = "assets/icons/ic_water.png";
+  static const String earth_element = "assets/icons/ic_earth.png";
+  static const String fire_element = "assets/icons/ic_fire.png";
+  static const String air_element = "assets/icons/ic_air.png";
+  static const String lion = "assets/icons/ic_lion.png";
+  static const String badger = "assets/icons/ic_badger.png";
+  static const String serpent = "assets/icons/ic_serpent.png";
+  static const String eagle = "assets/icons/ic_eagle.png";
+  static const String ghost = "assets/icons/ic_ghost.png";
+  static const String couch = "assets/icons/ic_couch.png";
 }
--- lib/utils/house_styles.dart
+++ lib/utils/house_styles.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
-
+import 'package:flutter/material.dart';
 import 'package:wizard_world/data/entities/house.dart';
+import 'package:wizard_world/presentation/components/assets/app_icons.dart';
 import 'package:wizard_world/presentation/components/assets/app_images.dart';
 
 extension HouseStyles on House {
@@ -22,13 +23,63 @@ extension HouseStyles on House {
   Color get houseColor {
     switch (name) {
       case "Gryffindor":
-        return const Color.fromARGB(255, 180, 24, 13);
+        return const Color.fromARGB(183, 255, 17, 0);
       case "Slytherin":
-        return const Color.fromARGB(255, 0, 117, 4);
+        return const Color.fromARGB(162, 0, 156, 5);
       case "Hufflepuff":
-        return const Color.fromARGB(255, 236, 174, 3);
+        return const Color.fromARGB(188, 219, 166, 18);
       case "Ravenclaw":
-        return const Color.fromARGB(255, 9, 98, 170);
+        return const Color.fromARGB(216, 8, 106, 187);
+      default:
+        throw UnimplementedError();
+    }
+  }
+
+  List<Color> get houseColorsArray {
+    switch (name) {
+      case "Gryffindor":
+        return const <Color>[
+          // Scarlet
+          Color(0xffFF2400),
+          // Gold
+          Color(0xffFFD700),
+        ];
+      case "Slytherin":
+        return const <Color>[
+          // Green
+          Color(0xff00FF00),
+          // Silver
+          Color(0xffC0C0C0),
+        ];
+      case "Hufflepuff":
+        return const <Color>[
+          // Yellow
+          Color(0xffFFFF00),
+          // Black
+          Color(0xff000000),
+        ];
+      case "Ravenclaw":
+        return const <Color>[
+          // Blue
+          Color(0xff0000FF),
+          // Bronze
+          Color(0xffCD7F32),
+        ];
+      default:
+        throw UnimplementedError();
+    }
+  }
+
+  String get animalImagePath {
+    switch (name) {
+      case "Gryffindor":
+        return AppIcons.lion;
+      case "Slytherin":
+        return AppIcons.serpent;
+      case "Hufflepuff":
+        return AppIcons.badger;
+      case "Ravenclaw":
+        return AppIcons.eagle;
       default:
         throw UnimplementedError();
     }
