f56792bd9fec3febb2b7ae78b640cd49e407d778
LodesHBeyers <lodesbeyers1@gmail.com>
2024-03-02 11:37:32 +0200

    Add comments to sizes

--- lib/utils/app_sizes.dart
+++ lib/utils/app_sizes.dart
@@ -6,10 +6,18 @@ class AppSizes {
 
   static double sh (BuildContext context) => MediaQuery.of(context).size.height;
 
-
+  /// 4
   static const double xxs = 4;
+  /// 8
   static const double xs = 8;
+  /// 16
   static const double s = 16;
+  /// 20
   static const double m = 20;
+  /// 24
   static const double l = 24;
+  /// 28
+  static const double xl = 28;
+  /// 32
+  static const double xxl = 32;
 }
