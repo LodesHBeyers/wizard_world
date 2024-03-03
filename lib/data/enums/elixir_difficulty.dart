9b033a667291a3d72c375e5cbd6e9ce620c65e76
LodesHBeyers <lodesbeyers1@gmail.com>
2024-03-03 12:58:53 +0200

    Embellish enum

--- lib/data/enums/elixir_difficulty.dart
+++ lib/data/enums/elixir_difficulty.dart
@@ -1,12 +1,30 @@
 // ignore_for_file: constant_identifier_names
 
 enum ElixirDifficulty {
-  Unknown,
-  Advanced,
-  Moderate,
-  Beginner,
-  OrdinaryWizardingLevel,
-  OneOfAKind;
+  Unknown(
+    "Unknown",
+  ),
+  Advanced(
+    "Advanced",
+  ),
+  Moderate(
+    "Moderate",
+  ),
+  Beginner(
+    "Beginner",
+  ),
+  OrdinaryWizardingLevel(
+    "Ordinary wizarding level",
+  ),
+  OneOfAKind(
+    "One of a kind",
+  );
+
+  final String localizedValue;
+
+  const ElixirDifficulty(
+    this.localizedValue,
+  );
 
   static ElixirDifficulty parse(String value) {
     return values.firstWhere(
