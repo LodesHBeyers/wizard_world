6b342f1612735fe306a76f427ed196455cd512ca
LodesHBeyers <lodesbeyers1@gmail.com>
2024-03-03 11:14:22 +0200

    embellsih enums

--- lib/data/enums/spell_light.dart
+++ lib/data/enums/spell_light.dart
@@ -1,36 +1,128 @@
 // ignore_for_file: constant_identifier_names
+import 'package:flutter/material.dart';
 
 enum SpellLight {
-  None,
-  Blue,
-  IcyBlue,
-  Red,
-  Gold,
-  Purple,
-  Transparent,
-  White,
-  Green,
-  Orange,
-  Yellow,
-  BrightBlue,
-  Pink,
-  Violet,
-  BlueishWhite,
-  Silver,
-  Scarlet,
-  Fire,
-  FieryScarlet,
-  Grey,
-  DarkRed,
-  Turquoise,
-  PsychedelicTransparentWave,
-  BrightYellow,
-  BlackSmoke;
+  None(
+    "None",
+    Colors.white,
+  ),
+  Blue(
+    "Blue",
+    Colors.blue,
+  ),
+  IcyBlue(
+    "Icy-blue",
+    Color.fromARGB(255, 119, 194, 255),
+  ),
+  Red(
+    "Red",
+    Colors.red,
+  ),
+  Gold(
+    "Gold",
+    Color(0xffFFD700),
+  ),
+  Purple(
+    "Purple",
+    Colors.purple,
+  ),
+  Transparent(
+    "Transparent",
+    Colors.transparent,
+  ),
+  White(
+    "White",
+    Colors.white,
+  ),
+  Green(
+    "Green",
+    Colors.green,
+  ),
+  Orange(
+    "Orange",
+    Colors.orange,
+  ),
+  Yellow(
+    "Yellow",
+    Colors.yellow,
+  ),
+  BrightBlue(
+    "Bright blue",
+    Color.fromARGB(255, 67, 170, 255),
+  ),
+  Pink(
+    "Pink",
+    Colors.pink,
+  ),
+  Violet(
+    "Violet",
+    Color(0xff7F00FF),
+  ),
+  BlueishWhite(
+    "Bluish white",
+    Color.fromARGB(255, 212, 235, 255),
+  ),
+  Silver(
+    "Silver",
+    Color(0xffC0C0C0),
+  ),
+  Scarlet(
+    "Scarlet",
+    Color.fromARGB(255, 255, 34, 0),
+  ),
+  Fire(
+    "Fire",
+    Color.fromARGB(255, 214, 79, 0),
+  ),
+  FieryScarlet(
+    "Fiery Scarlet",
+    Color.fromARGB(255, 255, 31, 15),
+  ),
+  Grey(
+    "Grey",
+    Colors.grey,
+  ),
+  DarkRed(
+    "Dark red",
+    Color.fromARGB(255, 128, 8, 0),
+  ),
+  Turquoise(
+    "Turquoise",
+    Color(0xff40E0D0),
+  ),
+  PsychedelicTransparentWave(
+    "Psychedelic transparent wave",
+    Colors.transparent,
+  ),
+  BrightYellow(
+    "Bright yellow",
+    Colors.yellow,
+  ),
+  BlackSmoke(
+    "Black smoke",
+    Colors.black,
+  );
+
+  final String localizedValue;
+  final Color color;
+
+  const SpellLight(
+    this.localizedValue,
+    this.color,
+  );
 
   static SpellLight parse(String value) {
     return values.firstWhere(
       (SpellLight element) => element.name == value,
       orElse: () => None,
     );
+  }
+
+  /// Whether or not the SpellLight color is visible
+  bool get isVisible {
+    return switch (this) {
+      PsychedelicTransparentWave || Transparent || White || None => false,
+      _ => true,
+    };
   }
 }
--- lib/data/enums/spell_type.dart
+++ lib/data/enums/spell_type.dart
@@ -1,24 +1,29 @@
 // ignore_for_file: constant_identifier_names
 
 enum SpellType {
-  None,
-  Charm,
-  Conjuration,
-  Spell,
-  Transfiguration,
-  HealingSpell,
-  DarkCharm,
-  Jinx,
-  Curse,
-  MagicalTransportation,
-  Hex,
-  CounterSpell,
-  DarkArts,
-  CounterJinx,
-  CounterCharm,
-  Untransfiguration,
-  BindingMagicalContract,
-  Vanishment;
+  None("None"),
+  Charm("Charm"),
+  Conjuration("Conjuration"),
+  Spell("Spell"),
+  Transfiguration("Transfiguration"),
+  HealingSpell("Healing spell"),
+  DarkCharm("Dark charm"),
+  Jinx("Jinx"),
+  Curse("Curse"),
+  MagicalTransportation("Magical transportation"),
+  Hex("Hex"),
+  CounterSpell("Counter spell"),
+  DarkArts("Dark arts"),
+  CounterJinx("Counter jinx"),
+  CounterCharm("Counter charm"),
+  Untransfiguration("Untransfiguration"),
+  BindingMagicalContract("Binding magical contract"),
+  Vanishment("Vanishment");
+
+  final String localizedValue;
+  const SpellType(
+    this.localizedValue,
+  );
 
   static SpellType parse(String value) {
     return values.firstWhere(
