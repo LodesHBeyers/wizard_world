fcc15268d983859d07001f61b857e2500ab5b7f8
LodesHBeyers <lodesbeyers1@gmail.com>
2024-03-04 19:05:23 +0200

    Home drawer tests

---
+++ test/presentation/screens/home/widgets/home_drawer_test.dart
@@ -0,0 +1,25 @@
+
+import 'package:given_when_then/given_when_then.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:wizard_world/presentation/screens/home/widgets/home_drawer.dart';
+
+import '../../../../test_utils/test_harness.dart';
+
+void main() {
+  testGoldens(
+    "Given HomeNavigationCard.houses When rendered Then display correct UI",
+    testHarness(
+      (
+        WidgetTestGiven<UIWidgetTestHarness> given,
+        WidgetTestWhen<UIWidgetTestHarness> when,
+        WidgetTestThen<UIWidgetTestHarness> then,
+      ) async {
+        await given.setupWidget(
+          const HomeDrawer(),
+        );
+
+        await then.goldensVerified("home_drawer");
+      },
+    ),
+  );
+}
